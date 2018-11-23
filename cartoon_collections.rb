bfb2d7632aba254918e6b438f1d55835dfad68f7
Matthias Rudin <matthias.rudin@live.com>
2018-11-23 04:37:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,5 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.map {|element| element == "cheddar" || "gouda" || "camembert" ? return element : nil}
+  return array.map {|element| element == "cheddar" || "gouda" || "camembert" ? element : nil}
 end
