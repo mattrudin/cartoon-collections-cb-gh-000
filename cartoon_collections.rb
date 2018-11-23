daf9af4000cc966db502461eb23e7d834c220b18
Matthias Rudin <matthias.rudin@live.com>
2018-11-23 04:25:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(array)
   array.map {|element| "#{element.slice(0,1).capitalize + element.slice(1..-1)}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|element| element.length > 4}
 end
 
 def find_the_cheese# code an argument here
